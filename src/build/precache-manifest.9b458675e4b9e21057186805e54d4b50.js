d7910556dc57f5e390681b3ad31a1e3840b56da1
Quan Laptop <globallivingstore@gmail.com>
2020-10-15 22:18:51 -0700

    1st commit

---
+++ src/build/precache-manifest.9b458675e4b9e21057186805e54d4b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b836ad36ca040473b4fe8ddf0cbb09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88650b46b2215f24be63",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88650b46b2215f24be63",
+    "url": "/static/js/main.78630205.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
